1ed01c69e6cb757feaf790120b6d7b8b7668aafb
Clarence Walker <eclarwa1@gmail.com>
2024-10-29 13:43:52 -0400

    Scripts

---
+++ venv/Scripts/io_test.py
@@ -0,0 +1,31 @@
+import time
+
+user_name = input("What is your name?? ")
+
+print("Hello, my name is {}-".format(user_name))
+
+time.sleep(2)
+
+print("IIIHHTTTTT DOESN'T MATTER WHAT YOUR NAME IS!")
+
+time.sleep(2.5)
+
+print("IFFF YOUUU SMEEEEEEEEEEEELLLLLLLELELELELELELELELEELLLLAAAAUUUU")
+
+time.sleep(4.5)
+
+print("WHAT THE ROCK")
+
+time.sleep(1)
+
+print("IS")
+
+time.sleep(1)
+
+print("COOKIN! *eyebrow*")
+
+time.sleep(.75)
+
+print(" ")
+
+print("*GLORIOUS THEME MUSIC*")
